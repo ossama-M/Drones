6363ef91ea00130f6c33c76ec870f40d50eebc10
Ossama Mohamed <o.moustafa@elsewedy.com>
2023-03-08 12:00:44 +0200

    Initial Application with Dependency

---
+++ src/test/java/com/Alzain/Drones/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alzain.Drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
